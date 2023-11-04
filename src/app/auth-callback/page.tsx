fb8425c40aa4c0769870fd8b2868c0e92c7cf89c
Alex <alex@q-83.com>
2023-11-04 23:27:42 +1100

    fix client issue

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from "react";
 
 import { useParams, useRouter, useSearchParams } from "next/navigation";
@@ -6,7 +8,6 @@ import { useParams, useRouter, useSearchParams } from "next/navigation";
 interface AuthCallbackProps {}
 
 const AuthCallbackPage = ({}: AuthCallbackProps) => {
-  console.log('auth callback page')
   const router = useRouter();
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
