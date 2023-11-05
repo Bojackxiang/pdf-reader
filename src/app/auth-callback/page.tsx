81bdda9d6f034c73158708684e354df79561c7ae
Alex <alex@q-83.com>
2023-11-05 18:13:15 +1100

    handle auth callback page

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,20 +1,57 @@
-'use client';
-
-import React from "react";
-
-import { useParams, useRouter, useSearchParams } from "next/navigation";
-
+import { redirect, useSearchParams } from "next/navigation";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { db } from "@/db";
+import { Loader2 } from "lucide-react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { toast } from "react-hot-toast";
+import { useEffect } from "react";
 
 interface AuthCallbackProps {}
 
-const AuthCallbackPage = ({}: AuthCallbackProps) => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const origin = searchParams.get("origin");
+const AuthCallbackPage = async ({}: AuthCallbackProps) => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
-  <div>auth callback</div>
+  // handle if no user
+  if (!user || !user.email || !user.id) {
+    // handle auth err: sign out
+    return;
+  }
 
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  });
 
+  if (!dbUser) {
+    const response = await db.user.create({
+      data: {
+        email: user.email,
+        id: user.id,
+      },
+    });
+
+    console.log(response);
+    if (response.id) {
+      redirect("/dashboard");
+    } else {
+      // handler error page
+      // sign out
+      return;
+    }
+  }
+
+  return (
+    <div className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+        <h3 className="font-semibold text-xl">Setting up your account...</h3>
+        <p>You will be redirected automatically.</p>
+      </div>
+    </div>
+  );
 };
 
 export default AuthCallbackPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "@/components/Navbar";
 import QueryProvider from "@/providers/query-providers";
+import { ToastProvider } from "@/providers/toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
         )}
       >
         <QueryProvider>
+          <ToastProvider />
           <NavBar />
           {children}
         </QueryProvider>
