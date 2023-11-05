14acb96bb43a349cc5ddaa056efcb13ea8801f02
Alex <alex@q-83.com>
2023-11-05 23:15:02 +1100

    update UI

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 import { db } from "@/db";
+import Dashboard from "@/components/Dashboard";
 
 interface DashboardPageProps {}
 
@@ -23,7 +24,11 @@ const DashboardPage = async ({}: DashboardPageProps) => {
     redirect("/auth-callback?origin=dashboard");
   }
 
-  return <div>DashboardPage</div>;
+  return (
+    
+      <Dashboard />
+    
+  );
 };
 
 export default DashboardPage;
